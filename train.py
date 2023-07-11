8f77c92f82448349d1c3a5738fc94ed897024d60
Sameer Khan <sameerkhan7494@gmail.com>
2023-07-11 07:14:11 -0400

    Implementing bigram model

--- train.py
+++ train.py
@@ -1,13 +1,68 @@
+import torch
+import torch.nn as nn
+
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
-
 chars = sorted(list(set(text)))
-vocab_size = len(chars)
-
 stoi = {ch: i for i, ch in enumerate(chars)}
 itos = {i: ch for i, ch in enumerate(chars)}
 def encode(s): return [stoi[c] for c in s]
 def decode(l): return ''.join([itos[i] for i in l])
 
 
-print(encode("hii there"))
+data = torch.tensor(encode(text), dtype=torch.long)
+n = int(0.9 * len(data))
+train_data = data[:n]
+eval_data = data[n:]
+
+torch.manual_seed(1337)
+batch_size, block_size = 4, 8
+
+
+def get_batch(split):
+    data = train_data if split == 'train' else eval_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    return x, y
+
+
+xb, yb = get_batch('train')
+# print(xb.shape)
+# print(xb)
+# print("----")
+# print(yb.shape)
+# print(yb)
+
+
+class BigramLM(nn.Module):
+    def __init__(self, vocab_size):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+
+    def forward(self, idx, target=None):
+        logits = self.token_embedding_table(idx)
+        if target is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            target = target.view(B*T)
+            loss = nn.functional.cross_entropy(logits, target)
+
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens):
+        for _ in range(max_new_tokens):
+            logits, loss = self(idx)
+            logits = logits[:, -1, :]
+            probs = nn.functional.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, idx_next), dim=1)
+        return idx
+
+
+m = BigramLM(65)
+idx = torch.zeros((1, 1), dtype=torch.long)
+generation = m.generate(idx, max_new_tokens=100)[0]
+print(generation)
